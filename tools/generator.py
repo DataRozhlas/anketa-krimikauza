dd229c0033e1d0751c05c6b80c2184eb48540af1
datastory.cz <honza@datastory.cz>
2019-12-19 13:19:18 +0100

    styling, nova data, fotky

--- tools/generator.py
+++ tools/generator.py
@@ -1,16 +1,15 @@
 #%%
 import pandas as pd
 import json
+import markdown
 
 #%%
 d = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1nw06N-h6IMBYkfPNLwtxpHMOOJ_GOX5GpAyjowedfN5DIPUYfszCuTdgRAQCOohrDhQc3uja8d99/pub?gid=0&single=true&output=csv')
 
 #%%
+d.dropna(how='all', inplace=True)
 d.jmeno_souboru_fotka.fillna('face.jpg', inplace=True)
 d.fillna('', inplace=True)
-
-#%%
-d.columns
 
 #%%
 d.rename(columns={
@@ -26,9 +25,8 @@ d.rename(columns={
     'jmeno_souboru_fotka': 'f'
 }, inplace=True)
 #%%
-
+d[['k1', 'd1', 'k2', 'd2', 'k3', 'd3']] = d[['k1', 'd1', 'k2', 'd2', 'k3', 'd3']].applymap(lambda x: markdown.markdown(x).replace('<p>', '').replace('</p>', ''))
 
 #%%
 with open('./data/data.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(list(d.to_dict(orient='index').values()),  ensure_ascii=False))
-
